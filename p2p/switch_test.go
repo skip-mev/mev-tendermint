4fb227cb4346974f52178cdc6be6dd7c34628ab4
William Shao <wllmshao@gmail.com>
2022-12-16 16:19:38 -0500

    Lookup IP every time we reconnect to relayer (#86)

--- p2p/switch_test.go
+++ p2p/switch_test.go
@@ -846,19 +846,19 @@ func BenchmarkSwitchBroadcast(b *testing.B) {
 	b.Logf("success: %v, failure: %v", numSuccess, numFailure)
 }
 
-func TestAddRelayerPeer(t *testing.T) {
+func TestSetRelayerPeer(t *testing.T) {
 	sw := MakeSwitch(cfg, 1, "testing", "123.123.123", initSwitchFunc)
 	relayerString := "79044d1d81d24a8ff3c7fd7e010f455f7ae9e1ad@1.2.3.4:26656"
 	relayerNetAddr, _ := NewNetAddressString(relayerString)
 
-	err := sw.AddRelayerPeer(relayerString)
+	err := sw.SetRelayerPeer(relayerString)
 	if err != nil {
-		t.Errorf("Err in AddRelayerPeer: %s", err)
+		t.Errorf("Err in SetRelayerPeer: %s", err)
 	}
 
 	assert.Equal(t, relayerNetAddr, sw.RelayerNetAddr, "Expected RelayerNetAddr %s, got %s", relayerNetAddr, sw.RelayerNetAddr)
 
 	errRelayerString := "abcd@1.2.3.4:26656"
-	err = sw.AddRelayerPeer(errRelayerString)
+	err = sw.SetRelayerPeer(errRelayerString)
 	assert.True(t, err != nil, "Expected err with invalid relayer string")
 }
